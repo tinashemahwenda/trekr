828fd2a189a11648583480140d10b2aebe0bad01
Tinashe Mahwenda <tinashemahwenda@gmail.com>
2024-01-01 08:42:32 +0200

    grabbed height and width of the device screen

--- lib/screens/trekr_search.dart
+++ lib/screens/trekr_search.dart
@@ -5,6 +5,8 @@ class TrekrSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
         body: SafeArea(
       child: Padding(
