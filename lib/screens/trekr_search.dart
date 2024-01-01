319953a50d3af481e97b43f8f6558cb7f4662603
Tinashe Mahwenda <tinashemahwenda@gmail.com>
2024-01-01 08:24:06 +0200

    Made a search screen entry code with a simple text widget at the centre

---
+++ lib/screens/trekr_search.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TrekrSearch extends StatelessWidget {
+  const TrekrSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Trekr Search'),
+      ),
+    );
+  }
+}
