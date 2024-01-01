3772cc74e8ea8f8daa6519ff252ff3aec8bcfb49
Tinashe Mahwenda <tinashemahwenda@gmail.com>
2024-01-01 08:54:18 +0200

    created the product display page

---
+++ lib/screens/trekr_product.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TrekrProduct extends StatelessWidget {
+  const TrekrProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Your Product'),
+      ),
+    );
+  }
+}
