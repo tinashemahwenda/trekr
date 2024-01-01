ff41cb288b62a4b66f7b6ed7816685cc95a0f5db
Tinashe Mahwenda <tinashemahwenda@gmail.com>
2024-01-01 08:21:09 +0200

    Created a trekr app function housing trekr home

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(TrekrApp());
+}
+
+class TrekrApp extends StatelessWidget {
+  const TrekrApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TrekrHome(),
+    );
+  }
+}
