9791facbd1c28b7581c7d81f26927b04b4900322
Tinashe Mahwenda <tinashemahwenda@gmail.com>
2024-01-02 10:01:43 +0200

    styled the top bar

--- lib/screens/trekr_product.dart
+++ lib/screens/trekr_product.dart
@@ -6,12 +6,30 @@ class TrekrProduct extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Row(
-          children: [Text('Track your parcel \nwith Trekr')],
-        )
-      ],
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 40.0,
+          horizontal: 30.0,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Track your parcel \nwith Trekr',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.search),
+              ],
+            )
+          ],
+        ),
+      ),
     ));
   }
 }
